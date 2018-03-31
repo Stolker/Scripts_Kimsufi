900a23dc6b0e51139cec9a032fbb000c4e7d354a
Stolker <9852255+Stolker@users.noreply.github.com>
2018-03-31 11:40:49 +0200

    Update rTorrent_monitor.sh

--- rTorrent_monitor.sh
+++ rTorrent_monitor.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ ! -x ./xmlrpc2scgi.py ]
+if [ ! -x ./usr/share/scripts-perso/xmlrpc2scgi.py ]
 then
 wget https://raw.githubusercontent.com/rakshasa/rtorrent-vagrant/master/scripts/xmlrpc2scgi.py &> /dev/null
 chmod 0755 xmlrpc2scgi.py
 fi
-status=$(./xmlrpc2scgi.py -p scgi://127.0.0.1:5001 get_ip 2>/dev/null| grep -c "0.0.0.0")
+status=$(./usr/share/scripts-perso/xmlrpc2scgi.py -p scgi://127.0.0.1:5001 get_ip 2>/dev/null| grep -c "0.0.0.0")
 if [ $status -eq 0 ]
 then
 echo "$(date +"%D %T") : WARNING - rTorrent is stopping, please restart">>/var/log/rTorrent_monitor.log
